e673a2d13fcbf698c9ea70a716f9c94b0883c6ff
Shashwat Raj <rajayshashwat@gmail.com>
2025-10-27 00:39:08 -0700

    Clean up: Remove unused files

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Starting KnockScript Web Compiler..."
-echo "Port: ${PORT:-4567}"
-echo "Environment: ${RACK_ENV:-production}"
-
-# Start Puma with the configuration file
-bundle exec puma -C config/puma.rb config.ru
